2eae882eecb969745e58f2848bacd2e7364d46d9
impinianyi-sys <angie@fw-angela.myguest.virtualbox.org>
2026-03-23 10:36:55 +0100

    Conexion exitosa con la base de datos usando una pdo

---
+++ configDB.php
@@ -0,0 +1,21 @@
+<?php
+
+try {
+	$pdo = new PDO (
+		"mysql:host=localhost;dbname=PHP_GBA;charset=utf8mb4",
+		"admin_GBA",
+		"Asdqwe123.",
+     [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,  
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
+        PDO::ATTR_EMULATE_PREPARES   => false,                  
+    ]);
+
+    echo "Connexió exitosa";
+
+} catch (PDOException $e) {
+    
+    error_log("Error de conexión: " . $e->getMessage()); 
+    die("No s'ha pogut connectar a la base de dades"); 
+}
+?>
