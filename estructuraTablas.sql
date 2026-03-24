5861079708cb8cb0f51b2d343b49a5a7574e4e95
impinianyi-sys <angie@fw-angela.myguest.virtualbox.org>
2026-03-24 12:36:35 +0100

    actualizacion de la base de datos

--- estructuraTablas.sql
+++ estructuraTablas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 23-03-2026 a las 08:58:15
+-- Tiempo de generación: 24-03-2026 a las 12:15:47
 -- Versión del servidor: 8.0.45-0ubuntu0.22.04.1
 -- Versión de PHP: 8.1.2-1ubuntu2.23
 
@@ -72,8 +72,22 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `contraseña` varchar(255) NOT NULL,
-  `rol` enum('admin','usuario') NOT NULL DEFAULT 'usuario'
+  `rol` enum('admin','abogado','cliente') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT 'cliente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `email`, `contraseña`, `rol`) VALUES
+(1, 'hola', 'hola@gmail.com', '$2y$10$Ctc.wx/lf..sIYUTnIJNpum.8chSvZ9hYM45f7GVp4UORZNiJzOWi', 'cliente'),
+(2, 'Abogado', 'abogado@gmail.com', '$2y$10$RA71Fq3OxvIWfLb.4jbbVurH0mdA6ot5JBSL1BIcuFLdgwCpOvgA6', 'cliente'),
+(3, 'test', 'test@gmail.com', '$2y$10$jomnQNH3nMWeU3vAOfdsYOLXQQqy0MDB5ageEuyNMkiqarwIOsID.', 'cliente'),
+(4, 'abotest', 'abotest@gmail.com', '$2y$10$lXOzCMrARpBpYnynHFCNkuYX.SDK1G.qj7yhc5MWfh1/bznJWAab.', 'cliente'),
+(5, 'admin_GBA', 'impinianyi@gmail.com', '$2y$10$qbCXW9bVs/p7p0xVzdwdverle4Q3wyedRJ7S4dFnLXNKssoKyvpaC', 'admin'),
+(6, 'test1', 'test1@gmail.com', '$2y$10$Q8j.8tzdphU3MMeGa6glSuAntpV3xS54gzMf.MoZjMv/yXU9iHGjK', 'cliente'),
+(7, 'holaa', 'holaa@gmail.com', '$2y$10$/nROOGObIHwf5SfORCxUBuqL/xleGZ1D0VZiINkAAHYefcmJvWz4S', 'cliente'),
+(8, 'Xavi garcia', 'xgarcia@gmail.com', '$2y$10$xc.Y4/Y/ZpzrQbkCJ8BDneLigpS1CBkLy0ji2lMYzIHQOQHJrTqa2', 'abogado');
 
 --
 -- Índices para tablas volcadas
@@ -132,7 +146,7 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
